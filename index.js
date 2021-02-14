07b82330dfcc388283ab8c66a21355551adf3320
Gids <gidsgoldberg@gmail.com>
2021-02-14 16:58:25 +0000

    fixup

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 'use strict';
 import * as core from '@actions/github';
 
-const context = core.core.github.context;
+const context = core.context;
 console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
