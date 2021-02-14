4352ec25b183661cb4e81b889f7b1df352d8918a
Gids <gidsgoldberg@gmail.com>
2021-02-14 16:56:14 +0000

    fixup

--- index.js
+++ index.js
@@ -2,8 +2,5 @@
 'use strict';
 import * as core from '@actions/github';
 
-const myInput = core.core.getInput('who-to-greet');
-
-
-const context = github.context;
-console.log(`We can even get context data, like the repo: ${core.context.repo.repo}`)
+const context = core.core.github.context;
+console.log(`We can even get context data, like the repo: ${context.repo.repo}`)
